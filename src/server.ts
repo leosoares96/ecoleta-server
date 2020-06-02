843a738b39b28e36aaefe8be7e7dc66502ef1e38
Leonardo Soares <leo.soares64@outlook.com>
2020-06-02 16:16:16 -0300

    criaçao de conexao com bd

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,10 @@
 import express from 'express';
+import routes from './routes'
 
 const app = express();
-app.use(express.json())
 
-const users = [
-    'Leonardo',
-    'Teste 01',
-    'Teste 02',
-    'Daniel'
-]
+app.use(express.json());
 
-app.get('/users', (request, response) =>{
-    const search = String(request.query.search)
-    const filteredUsers = users.filter(user => user.includes(search))
-    response.json(filteredUsers);
-});
-
-app.get('/users', (request, response) =>{
-    response.json(users);
-});
-
-app.get('/users/:id', (request, response) =>{
-    const id = Number(request.params.id);
-    response.json(users[id]);
-});
-
-app.post('/users', (request, response) =>{
-    const data = request.body;
-
-    console.log(data);
-    
-    const user = {
-        name: data.name,
-        email: data.email,
-    };
-
-    return response.json(user)
-
-})
-
+app.use(routes);
 
 app.listen(3333);
