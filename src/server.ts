2275a2c6b6a2baadfdb49c83b6ee319a3328ae69
Leonardo Soares <leo.soares64@outlook.com>
2020-06-02 15:56:16 -0300

    testando tipos de requisiçoes da api

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,43 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json())
+
+const users = [
+    'Leonardo',
+    'Teste 01',
+    'Teste 02',
+    'Daniel'
+]
 
 app.get('/users', (request, response) =>{
-    console.log('Listagem de usuarios');
+    const search = String(request.query.search)
+    const filteredUsers = users.filter(user => user.includes(search))
+    response.json(filteredUsers);
+});
 
+app.get('/users', (request, response) =>{
+    response.json(users);
+});
+
+app.get('/users/:id', (request, response) =>{
+    const id = Number(request.params.id);
+    response.json(users[id]);
+});
+
+app.post('/users', (request, response) =>{
+    const data = request.body;
+
+    console.log(data);
     
-    response.json([
-        'Leonardo',
-        'Teste 01',
-        'Teste 02',
-        'Daniel',
-        'teste'
-    ]);
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+
+    return response.json(user)
+
 })
+
 
 app.listen(3333);
